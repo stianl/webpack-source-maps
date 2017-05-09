6cfb0ee028359f182e72df054d93c67c923cae42
Stian Lindhom <stian@lindhom.no>
2017-05-09 21:23:28 +0200

    Test

---
+++ bar.js
@@ -0,0 +1,8 @@
+export const parent = 'bar'
+export const test = 'ing'
+export const parent2 = 'bar'
+export const test2 = 'ing'
+export const parent3 = 'bar'
+export const test3 = 'ing'
+export const parent4 = 'bar'
+export const test4 = 'ing'
---
+++ index.js
@@ -0,0 +1,10 @@
+import bar from './foo'
+import * as bar2 from './foo'
+import { parent } from './bar'
+import { test } from './bar'
+import { parent3 } from './bar'
+import { test3 } from './bar'
+import { parent2 } from './bar'
+import { test2 } from './bar'
+
+console.log(bar, bar2, parent, test)
